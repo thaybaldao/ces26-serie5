6d20dcafe0bec49c8476bf181d90af97feebdef6
thaybaldao <thayna.baldao@gmail.com>
2020-10-31 02:08:55 -0300

    adding status to responses

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 // enable CORS
 app.use(cors());
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -21,7 +22,6 @@ var file = "data.json";
 app.post("/submit", async (req, res) => {
   try {
     var obj = req.body;
-
     fs.readFile(file, (err, data) => {
       if (err && err.code === "ENOENT") {
           // the file might not yet exist.  If so, just write the object and bail
@@ -40,7 +40,7 @@ app.post("/submit", async (req, res) => {
           }
       }
   });
-    res.json(req.body);
+    res.status(200).json(req.body);
   } catch (err) {
     console.error(err.message);
   }
@@ -55,7 +55,7 @@ app.get("/submissions", async (req, res) => {
           message: "There are no form submissions."
         });
       }
-      res.json(JSON.parse(data));
+      res.status(200).json(JSON.parse(data));
     });
   } catch (err) {
     console.error(err.message);
